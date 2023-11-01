ead2ebc51fc929c6892f7eab348540d95896558e
김주찬 <jckim2@gimjuchan-ui-MacBookAir.local>
2023-11-02 00:16:33 +0900

    feat(Test):  우승자 결정 테스트 코드 추가 -  findWinner 테스트 코드 추가

--- src/test/java/racingcar/domain/DomainTest.java
+++ src/test/java/racingcar/domain/DomainTest.java
@@ -14,15 +14,23 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class DomainTest extends NsTest {
     String inputString = "juch,jayo,hi";
     String[] result = inputString.split(",");
+    CarEntity isWinnerCar = new CarEntity("thr", 7);
 
     CarEntity[] arrayExCars = {new CarEntity("one", 4),
-                               new CarEntity("two", 6),
-                               new CarEntity("thr", 7)};
+            new CarEntity("two", 6),
+            isWinnerCar};
     List<CarEntity> exCars = new ArrayList<>(List.of(arrayExCars));
+    List<CarEntity> winners = new ArrayList<>();
 
     @Test
-    void Max_Score_반환() {
+    void maxScore_반환() {
         assertThat(FindMaxScore.findMax(exCars)).isEqualTo(7);
+    }
+
+    @Test
+    void findWinner_우승자_반환() {
+        winners.add(isWinnerCar);
+        assertThat(FindWinnerCar.equalMaxScroe(exCars, 7)).isEqualTo(winners);
     }
 
     @Override
